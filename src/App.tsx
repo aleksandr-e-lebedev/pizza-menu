b6b6e01e70310ffb185696847b646bc55be261cb
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-03-31 14:55:51 +0300

    feat: build Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header';
+import Menu from '@/components/Menu';
 
 export default function App() {
   return (
     <div className="container">
       <Header />
+      <Menu />
     </div>
   );
 }
