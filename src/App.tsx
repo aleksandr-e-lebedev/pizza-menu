3dfe39f75e9f5fec0ee6d2e2de252524828414f7
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-03-31 12:31:50 +0300

    feat: build app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from '@/components/Header';
+
 export default function App() {
   return (
     <div className="container">
-      <h1>Pizza Menu</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Pizza Menu</h1>
+    </header>
+  );
+}
